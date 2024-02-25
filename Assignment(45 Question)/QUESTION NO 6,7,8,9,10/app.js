a89a6a5e2fd45246567efac805c9768226a04cb9
Muhammad Amsal <muhammadamsal526@gmail.com>
2024-02-25 21:21:33 +0500

    question 6 to 10 com

--- Assignment(45 Question)/QUESTION NO 6,7,8,9,10/app.js
+++ Assignment(45 Question)/QUESTION NO 6,7,8,9,10/app.js
@@ -1,3 +1,33 @@
 "use strict";
+// QUESTION NO 6
 let personName = "    \n\tEllon Musk\t\n    ";
 console.log(personName);
+let name_Without_White_Space = personName.trim();
+console.log(name_Without_White_Space);
+// QUESTION NO 7,8
+console.log(4 + 4);
+console.log(12 - 4);
+console.log(4 * 2);
+console.log(16 / 2);
+//QUESTION NO 9
+let favouriteNumber = 7;
+let message = `My Favourite Number is ${favouriteNumber}`;
+console.log(message);
+//QUESTION NO 10
+// THIS PROGRAM IS WRITTEN BY AMSAL
+// DATE:19-2-24
+// THIS PROGRAM ADD TWO NUMBER
+let num1 = 4;
+let num2 = 6;
+console.log(num1 + num2);
+// THIS PROGRAM IS WRITTEN BY AMSAL
+// DATE:19-2-24
+// THIS PROGRAM PRINTS EVEN NUMBER & ODD NUMBER FROM 1 to 10
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(`${i} is an EVEN Number`);
+    }
+    else {
+        console.log(`${i} is an ODD Number`);
+    }
+}
