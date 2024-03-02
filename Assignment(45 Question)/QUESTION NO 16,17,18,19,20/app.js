4817feb353281f7807c37dc242698879b1049dc2
Muhammad Amsal <muhammadamsal526@gmail.com>
2024-03-02 23:22:42 +0500

    q no 16 to 20 com

--- Assignment(45 Question)/QUESTION NO 16,17,18,19,20/app.js
+++ Assignment(45 Question)/QUESTION NO 16,17,18,19,20/app.js
@@ -30,3 +30,31 @@ for (let i = 0; i < guestList.length; i++) {
 }
 guestList.splice(0, 2);
 console.log(guestList);
+// QUESTION NO 19
+console.log(`The no of invited people are : ${guestList.length}`);
+// QUESTION NO 18
+let places = ["Brazil", "Australia", "Denmark", "China"];
+// Sorted in Alphabetical order
+console.log(`Original Array :${places}`);
+let sorted_Array = [...places].sort();
+console.log(`In Alphabetical Order:${sorted_Array}`);
+// Sorted in Reverse alphabetical order
+console.log(`Original Array :${places}`);
+let reverseAlphabetical = [...places].sort((a, b) => b.localeCompare(a));
+console.log(`In Reverse Alphabetical Order:${reverseAlphabetical}`);
+// Print in Reverse Order
+console.log(`Original Array :${places}`);
+let reversed_order = [...places].reverse();
+console.log(`Reverse Array: ${reversed_order}`);
+// reverse the order of your list again
+let againReverse = reversed_order.reverse();
+console.log(`Again Reversed : ${againReverse}`);
+//Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+console.log([...places].sort());
+// Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+console.log([...places].sort().reverse());
+// QUESTION NO 20
+let languages = ["urdu", "english", "spanish", "french", "parsi"];
+languages.map((e) => {
+    console.log(`• ${e}`);
+});
--- Assignment(45 Question)/QUESTION NO 16,17,18,19,20/app.ts
+++ Assignment(45 Question)/QUESTION NO 16,17,18,19,20/app.ts
@@ -40,3 +40,43 @@ console.log(`Dear ${guestList[i]}, \n\n I would like to invite dinner \n\n Thank
 }  
 guestList.splice(0,2)
 console.log(guestList)
+
+// QUESTION NO 19
+
+console.log(`The no of invited people are : ${guestList.length}`)
+
+// QUESTION NO 18
+
+let places: string[]=["Brazil","Australia","Denmark","China"]
+
+// Sorted in Alphabetical order
+
+console.log(`Original Array :${places}`)
+let sorted_Array=[...places].sort()
+console.log(`In Alphabetical Order:${sorted_Array}`)
+
+// Sorted in Reverse alphabetical order
+console.log(`Original Array :${places}`)
+let reverseAlphabetical=[...places].sort((a, b) => b.localeCompare(a));
+console.log(`In Reverse Alphabetical Order:${reverseAlphabetical}`)
+
+// Print in Reverse Order
+console.log(`Original Array :${places}`)
+let reversed_order=[...places].reverse()
+console.log(`Reverse Array: ${reversed_order}`)
+// reverse the order of your list again
+let againReverse=reversed_order.reverse()
+console.log(`Again Reversed : ${againReverse}`)
+
+//Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+console.log([...places].sort())
+
+// Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+console.log([...places].sort().reverse())
+
+// QUESTION NO 20
+
+let languages: string[]=["Urdu","English","Spanish","French","Parsi"]
+languages.map((e)=>{
+console.log(`• ${e}`)
+})
