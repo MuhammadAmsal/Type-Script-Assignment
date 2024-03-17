f772928971e7e8075f222ec8f8320af082242d29
Muhammad Amsal <muhammadamsal526@gmail.com>
2024-03-17 15:08:45 +0500

    30 questions com

--- Assignment(45 Question)/QUESTION NO 21 to 30/app.js
+++ Assignment(45 Question)/QUESTION NO 21 to 30/app.js
@@ -176,3 +176,33 @@ else if (age >= 20 && age < 65) {
 else if (age >= 65) {
     console.log("The person is elder");
 }
+// Question no 29
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+let fruits = ["Mango", "Banana", "Pineapple"];
+if (fruits.includes('Mango')) {
+    console.log('I like Mango');
+}
+if (fruits.includes('Banana')) {
+    console.log('I like Banana');
+}
+if (fruits.includes('Pineapple')) {
+    console.log('I like Pineapple');
+}
+if (fruits.includes('Cherry')) {
+    console.log('I like Cherry');
+}
+// Question no 30
+// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+let person = ["kevin", "john", "admin", "jack", "morgan"];
+person.map((e) => {
+    if (e === "admin") {
+        console.log(` Hello ${e}, would you like to see a status report?`);
+    }
+    else {
+        console.log(` Hello ${e}, thank you for logging in again.`);
+    }
+});
--- Assignment(45 Question)/QUESTION NO 21 to 30/app.ts
+++ Assignment(45 Question)/QUESTION NO 21 to 30/app.ts
@@ -175,44 +175,44 @@ if(aleinColor==="Green"){
 // • Write three versions of this program, making sure each message is printed for the appropriate color alien.
 
 // version 1
-// let aleinColor1:string="Green"
+let aleinColor1:string="Green"
 
-// if (aleinColor1==="Green"){
-//     console.log("You earn 5 points")
-// }else if(aleinColor1==="Yellow"){
-//     console.log("You earn 10 points")
-// }else if(aleinColor1==="Red"){
-//     console.log("You earn 15 points")
-// }else {
-//     console.log("Please select color")
-// }
+if (aleinColor1==="Green"){
+    console.log("You earn 5 points")
+}else if(aleinColor1==="Yellow"){
+    console.log("You earn 10 points")
+}else if(aleinColor1==="Red"){
+    console.log("You earn 15 points")
+}else {
+    console.log("Please select color")
+}
 
 // // version 2
 
-// aleinColor1="Yellow"
+aleinColor1="Yellow"
 
-// if (aleinColor1==="Green"){
-//     console.log("You earn 5 points")
-// }else if(aleinColor1==="Yellow"){
-//     console.log("You earn 10 points")
-// }else if(aleinColor1==="Red"){
-//     console.log("You earn 15 points")
-// }else {
-//     console.log("Please select color")
-// }
+if (aleinColor1==="Green"){
+    console.log("You earn 5 points")
+}else if(aleinColor1==="Yellow"){
+    console.log("You earn 10 points")
+}else if(aleinColor1==="Red"){
+    console.log("You earn 15 points")
+}else {
+    console.log("Please select color")
+}
 
 // // version 3
-// aleinColor1="Red"
+aleinColor1="Red"
 
-// if (aleinColor1==="Green"){
-//     console.log("You earn 5 points")
-// }else if(aleinColor1==="Yellow"){
-//     console.log("You earn 10 points")
-// }else if(aleinColor1==="Red"){
-//     console.log("You earn 15 points")
-// }else {
-//     console.log("Please select color")
-// }
+if (aleinColor1==="Green"){
+    console.log("You earn 5 points")
+}else if(aleinColor1==="Yellow"){
+    console.log("You earn 10 points")
+}else if(aleinColor1==="Red"){
+    console.log("You earn 15 points")
+}else {
+    console.log("Please select color")
+}
 
 
 
@@ -243,3 +243,47 @@ if(age < 2){
 } else if(age >=65){
     console.log("The person is elder")
 }
+
+// Question no 29
+
+// Favorite Fruit: Make a array of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your array.
+// • Make a array of your three favorite fruits and call it favorite_fruits.
+// • Write five if statements. Each should check whether a certain kind of fruit is in your array. If the fruit is in your array, the if block should print a statement, such as You really like bananas!
+
+let fruits: string[]=["Mango","Banana","Pineapple"]
+if(fruits.includes('Mango')){
+    console.log('I like Mango')
+}
+if(fruits.includes('Banana')){
+    console.log('I like Banana')
+}
+if(fruits.includes('Pineapple')){
+    console.log('I like Pineapple')
+}
+if(fruits.includes('Cherry')){
+    console.log('I like Cherry')
+}
+
+
+
+
+// Question no 30
+
+// Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:
+// • If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+// • Otherwise, print a generic greeting, such as Hello Eric, thank you for logging in again.
+
+let person:string[]=["kevin","john","admin","jack","morgan"]
+
+person.map((e)=>{
+    if(e==="admin"){
+        console.log(` Hello ${e}, would you like to see a status report?`)
+    }else{
+        console.log(` Hello ${e}, thank you for logging in again.`)
+    }
+
+})
+
+
+
+ 
