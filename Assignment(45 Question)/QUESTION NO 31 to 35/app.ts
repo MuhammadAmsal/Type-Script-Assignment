ae5966af0224e74c050e4fa749851f832c2fbbd9
Muhammad Amsal <muhammadamsal526@gmail.com>
2024-03-19 16:58:58 +0500

    q no 35 com

--- Assignment(45 Question)/QUESTION NO 31 to 35/app.ts
+++ Assignment(45 Question)/QUESTION NO 31 to 35/app.ts
@@ -35,4 +35,55 @@ if(currentUsers.map(e=>e.toLowerCase()).includes(lowerCase)){
 
 
 
+// QUESTION NO 33
 
+// Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+// • Store the numbers 1 through 9 in a array.
+// • Loop through the array.
+// • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+let numbers:number[]=[1,2,3,4,5,6,7,8,9]
+
+let ordinalEnding:string;
+
+for(let num of numbers){
+if (num===1){
+  ordinalEnding="st"
+}else if(num===2){
+  ordinalEnding="nd"
+}else if(num===3){
+  ordinalEnding="rd"
+}else{
+  ordinalEnding="th"
+}
+console.log(`${num}${ordinalEnding}`)
+}
+
+
+
+
+// QUESTION NO 34
+
+// Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+// • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+// • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+
+let pizzas:string[] =["Cheese Pizza","Sicilian Pizza","BBQ Pizza"]
+
+for(let i=0;i<pizzas.length;i++){
+  console.log(`I like ${pizzas[i]} .`)
+}
+console.log(`I really love pizza!`)
+
+
+
+
+// QUESTION NO 35
+
+// Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+
+let animals:string[]=["Dog","Horse","Wolf"]
+
+for (let e of animals){
+  console.log(`A ${e} would make a great pet`)
+}
+console.log("Any of these animals would make a great pet!")
